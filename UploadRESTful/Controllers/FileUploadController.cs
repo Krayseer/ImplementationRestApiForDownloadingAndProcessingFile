0524107ca8d84a3f283476f7b98c404aa3358d44
Krayseer <Evgeny.Svalukhin@urfu.me>
2022-06-27 03:18:06 +0500

    upload new project

---
+++ UploadRESTful/Controllers/FileUploadController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.RegularExpressions;
+using UploadRESTful.Models;
+
+namespace UploadRESTful.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        private static string? result;
+        private static int id = 0;
+
+        public IActionResult Index() => View();
+
+        [HttpPost]
+        [RequestSizeLimit(1048576)] // Ограничение в 1 Мб
+        public async Task<IActionResult> Index(IFormFile files)
+        {
+            result = "";
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), files.FileName);
+            using var stream = new FileStream(filePath, FileMode.Create);
+            await files.CopyToAsync(stream);
+            var builderText = new StringBuilder();
+            var reader = new StreamReader(files.OpenReadStream());
+            while (reader.Peek() >= 0) builderText.AppendLine(reader.ReadLine());
+            var text = Regex.Split(builderText.ToString(), @"\r\n");
+            result = Parser.ParseText(text); // Получить распарсенный файл в виде дерева
+            id = new Random().Next(1, 100);
+            return Ok("Твой id для получения информации: " + id);
+        }
+
+        [HttpGet("FileUpload/{id}")] // Получить текст по заданному id
+        public IActionResult GetText() => Ok(result);
+    }
+}
---
+++ UploadRESTful/Models/Parser.cs
@@ -0,0 +1,31 @@
+﻿namespace UploadRESTful.Models
+{
+    public class Parser
+    {
+        public static string ParseText(string[] text)
+        {
+            var result = "";
+            int fixedSectionNumber = 0;
+            foreach (var line in text)
+            {
+                int sectionNumber = 0;
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] != '#') break;
+                    sectionNumber++;
+                }
+
+                if (sectionNumber != 0)
+                {
+                    fixedSectionNumber = sectionNumber;
+                    result += "\n" + new string('\t', sectionNumber - 1) + line[sectionNumber..] + "\n";
+                }
+
+                else
+                    result += new string('\t', fixedSectionNumber) + line + "\n";
+            }
+            return result[1..]; // учитываю что самый первый символ # заменяется на \n - перевод строки
+                                // чтобы убрать пустую строку, использую срез
+        }
+    }
+}
